7dda2d186b8ba993d7ec86246bde3475b6729d36
Ariel Bentolila <ariel.bentolila@sap.com>
2017-10-19 11:20:31 +0300

    .

--- lib/java_buildpack/framework/hotswap_agent.rb
+++ lib/java_buildpack/framework/hotswap_agent.rb
@@ -37,6 +37,8 @@ module JavaBuildpack
       def compile
         download_jar('1.0', @configuration['uri'], hotswap_jar_name, libpath)
         download_jar('1.0', @configuration['vscodedebug_uri'], vscodedebug_jar_name, libpath)
+        download_jar('1.0', @configuration['vscodedebug_ms_jar_uri'], @configuration['vscodedebug_ms_jar_name'], libpath)
+        
         download_tar('1.0', @appcontroller_uri, true, libpath, 'App Controller')
 
         #download_tar('1.0', @jdblibs_uri, false, libpath, 'JDB')
